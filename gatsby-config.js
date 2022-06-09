76a40dcca8ca67e7283570e064bbfd19a25334f3
naveenavi <naveenj085@gmail.com>
2022-06-09 13:22:04 +0530

    Finished Gatsby Tutorial Part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,9 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: `https://www.yourdomain.tld`,
+    title: "Gatsby Site",
   },
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import * as React from 'react';
 import Layout from '../components/layout';
+import { StaticImage } from 'gatsby-plugin-image';
 
 const IndexPage = () => {
   return (
    <Layout pageTitle ="Home Page">
      <p>Gatsby Tutorial.</p>
+     <StaticImage
+        alt="camera"
+        src="../images/default.jpg"
+      />
    </Layout>
   )
 }
